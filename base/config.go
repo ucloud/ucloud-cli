82831a46f395abb1284a4785050fabd83be3f130
renzheng.wang <wangrzneu@gmail.com>
2024-09-20 08:49:55 +0800

    add change log

--- base/config.go
+++ base/config.go
@@ -39,7 +39,7 @@ const DefaultBaseURL = "https://api.ucloud.cn/"
 const DefaultProfile = "default"
 
 // Version 版本号
-const Version = "0.2.0"
+const Version = "0.3.0"
 
 var UserAgent = fmt.Sprintf("UCloud-CLI/%s", Version)
 
